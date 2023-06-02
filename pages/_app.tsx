c3147526b6bedb9091249b34ad55da0ff313dee0
jasnlink <chem9310@gmail.com>
2023-06-02 02:31:29 -0400

    defer google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,13 +21,16 @@ export default function App({ Component, pageProps }: AppProps) {
                     <Content>
                         {!!isProduction && (
                             <>
-                                <Script src="https://www.googletagmanager.com/gtag/js?id=G-YLMZM6JBQX" strategy="lazyOnload" />
-                                <Script id="google-analytics" strategy="lazyOnload">
+                                <Script defer src="https://www.googletagmanager.com/gtag/js?id=G-YLMZM6JBQX" strategy="lazyOnload" />
+                                <Script defer id="google-analytics" strategy="lazyOnload">
                                     {`
                                         window.dataLayer = window.dataLayer || [];
                                         function gtag(){dataLayer.push(arguments);}
                                         gtag('js', new Date());
                                         gtag('config', 'G-YLMZM6JBQX');
+                                        gtag('config', 'G-YLMZM6JBQX', {
+                                            page_path: window.location.pathname,
+                                        });
                                     `}
                                 </Script>
                             </>
