53f7f5ea8cba189f1b21a847258576c38334cd97
jasnlink <chem9310@gmail.com>
2023-07-19 02:42:27 -0400

    Moved to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,12 @@ import Layout from '@/src/components/Layout'
 import NavMenu from '@/src/components/NavMenu'
 import Content from '@/src/components/Content'
 
+import { Partytown } from '@builder.io/partytown/react'
+import Script from 'next/script'
+
 export default function App({ Component, pageProps }: AppProps) {
+
+    const isProduction = process.env.NODE_ENV === "production";
 
     return (
         <QueryClientProvider client={queryClient}>
@@ -15,6 +20,29 @@ export default function App({ Component, pageProps }: AppProps) {
                 <Layout>
                     <NavMenu />
                     <Content>
+                        {!!isProduction && (
+                            <>
+                                <Partytown debug={true} logScriptExecution={true} forward={['dataLayer', 'gtag']} />
+                                <Script
+                                    id="google-analytics"
+                                    type="text/partytown"
+                                    strategy="afterInteractive"
+                                    src="https://www.googletagmanager.com/gtag/js?id=G-YLMZM6JBQX"
+                                />
+                                <Script
+                                    id="google-tag"
+                                    type="text/partytown"
+                                    strategy="afterInteractive"
+                                >
+                                    {`
+                                        window.dataLayer = window.dataLayer || [];
+                                        window.gtag = function gtag(){window.dataLayer.push(arguments);}
+                                        gtag('js', new Date());
+                                        gtag('config', 'G-YLMZM6JBQX',{ 'debug_mode':true });
+                                    `}
+                                </Script>
+                            </>
+                        )}
                         <Component {...pageProps} />
                     </Content>
                 </Layout>
